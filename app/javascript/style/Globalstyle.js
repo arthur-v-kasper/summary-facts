e5161682b25f66b6140314af6f8310bcaf30f70d
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2021-07-10 09:49:50 -0300

    Add GlobalStyle

---
+++ app/javascript/style/Globalstyle.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  
+  html{
+    font-family: "Poppins", sans-serif;
+    font-weight: 300;
+    font-size: 100%;
+    color: #212121;
+    box-sizing: border-box;
+  }
+
+  *,*:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+  }
+`
+
+const GlobalStyleComponet = () => <GlobalStyle />
+
+export default GlobalStyleComponet;
